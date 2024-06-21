0c7b531db7e10c6655359a73aa5b5594e5805f74
Albert Anderberg <albert@aanet.se>
2024-06-21 11:04:27 +0200

    WIP

--- components/tle94110el/tle94110el.cpp
+++ components/tle94110el/tle94110el.cpp
@@ -4,10 +4,27 @@
 namespace esphome {
 namespace tle94110el {
 
-static const char *TAG = "tle94110el.component";
+static const char *TAG = "tle94110el";
 
 void tle94110el::setup() {
-    ESP_LOGI(TAG, "Test setup log");
+    ESP_LOGI(TAG, "Setup start");
+
+    this->spi_setup();
+    ESP_LOGI(TAG, "SPI setup complete");
+
+    this->enable();
+    ESP_LOGI(TAG, "SPI device enabled");
+
+    uint8_t data[] = {0b01100111, 0};
+
+    this->transfer_array(data, sizeof(data));
+    ESP_LOGI(TAG, "Array transfered");
+    ESP_LOGI(TAG, data[0]);
+    ESP_LOGI(TAG, data[1]);
+
+    this->disable();
+    ESP_LOGI(TAG, "Spi device disabled");
+
 }
 
 void tle94110el::loop() {
--- components/tle94110el/tle94110el.h
+++ components/tle94110el/tle94110el.h
@@ -4,17 +4,12 @@
 #include "esphome/components/spi/spi.h"
 
 namespace esphome {
-namespace tle94110el {
-
-class tle94110el : public Component, 
-                    public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST,spi::CLOCK_POLARITY_LOW, 
-                            spi::CLOCK_PHASE_LEADING,spi::DATA_RATE_1KHZ> {
-  public:
-    void setup() override;
-    void loop() override;
-    void dump_config() override;
-};
-
-
-}
+  namespace tle94110el {
+    class tle94110el : public Component, public spi::SPIDevice<spi::BIT_ORDER_LSB_FIRST,spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_TRAILING,spi::DATA_RATE_1MHZ> {
+      public:
+        void setup() override;
+        void loop() override;
+        void dump_config() override;
+    };
+  }
 }
