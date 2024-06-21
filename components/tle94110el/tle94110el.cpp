9826caba629acfb7ec9e3e43c0bbdd88f2ba0ab3
Albert Anderberg <albert@aanet.se>
2024-06-21 13:44:05 +0200

    WIP

--- components/tle94110el/tle94110el.cpp
+++ components/tle94110el/tle94110el.cpp
@@ -12,11 +12,54 @@ void tle94110el::setup() {
     this->spi_setup();
     ESP_LOGI(TAG, "SPI setup complete");
 
+
+    delay(1);
+
     this->enable();
     ESP_LOGI(TAG, "SPI device enabled");
 
     uint8_t data[] = {0b01100111, 0};
     char msg[20];	
+
+    this->transfer_array(data, sizeof(data));
+    ESP_LOGI(TAG, "Array transfered");
+    
+    snprintf(msg, sizeof(msg), "%u", data[0]);
+    ESP_LOGI(TAG, msg);
+
+    snprintf(msg, sizeof(msg), "%u", data[1]);
+    ESP_LOGI(TAG, msg);
+
+    this->disable();
+    ESP_LOGI(TAG, "Spi device disabled");
+
+
+    delay(1);
+
+    this->enable();
+    ESP_LOGI(TAG, "SPI device enabled");
+
+    uint8_t data1[] = {0b10011011, 0};
+    char msg1[20];	
+
+    this->transfer_array(data1, sizeof(data1));
+    ESP_LOGI(TAG, "Array transfered");
+    
+    snprintf(msg1, sizeof(msg1), "%u", data1[0]);
+    ESP_LOGI(TAG, msg1);
+
+    snprintf(msg1, sizeof(msg1), "%u", data1[1]);
+    ESP_LOGI(TAG, msg1);
+
+    this->disable();
+    ESP_LOGI(TAG, "Spi device disabled");
+
+
+
+    delay(1);
+
+    this->enable();
+    ESP_LOGI(TAG, "SPI device enabled");
 
     this->transfer_array(data, sizeof(data));
     ESP_LOGI(TAG, "Array transfered");
